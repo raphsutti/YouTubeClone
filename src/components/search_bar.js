39c348b09b1e93def49d3f3f5cead3bc0378c5a6
raphsutti <neoster9@gmail.com>
2018-04-29 20:10:38 +1000

    Search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,3 +3,5 @@ import React from 'react';
 const SearchBar = () => {
   return <input />
 }
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDYomOy8E018K4w5b5UxDQoImpGecVm5H4';
 
 // Create new component. This component should produce HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 // Put generated HTML into the DOM (onto the page)
