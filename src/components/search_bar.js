d0e758921753ae6f267f0483446a3176f79fbde1
raphsutti <neoster9@gmail.com>
2018-04-30 18:14:33 +1000

    comments and refactor

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
-    // run onInputChange() when onChange occurs
-    return <input onChange={this.onInputChange} />;
+  // state setup
+  constructor(props) {
+    super(props);
+    // outside constructor, use this.setState
+    this.state = { term: '' };
   }
 
-  // detect events
-  onInputChange() {
-
+  render() {
+    // value set to current state
+    // update 'term' state with user input using this.setState
+    // causes re-render and value updated
+    // show value using this.state
+    return (
+      <div>
+        <input 
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value }) }/>
+        Value of the input: { this.state.term }
+      </div>
+    );
   }
 }
 
