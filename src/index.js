d9d813902566b9bfdf4e77514c8fe656f5b8f334
raphsutti <neoster9@gmail.com>
2018-04-30 19:12:49 +1000

    add constructor for App, store state to videos

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDYomOy8E018K4w5b5UxDQoImpGecVm5H4';
 
-// YouTube search using key and search term
-YTSearch({key: API_KEY, term: 'corsair'}, function(data) {
-  console.log(data);
-});
-
 // Create new component. This component should produce HTML
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  );
+class App extends Component {
+  // Constructor function to update state when user input search
+  constructor(props) {
+    super(props);
+    
+    this.state = { };
+
+    // YouTube search using key and search term
+    YTSearch({ key: API_KEY, term: 'corsair' }, (videos) => {
+      // when key and value identical ie. videos: videos can condensed
+      // below same as this.setState({ videos: videos });
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 // Put generated HTML into the DOM (onto the page)
