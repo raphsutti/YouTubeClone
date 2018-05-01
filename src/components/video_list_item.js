5f2ffe6317c741a67fcdc3605c67f2b2e90a01b3
raphsutti <neoster9@gmail.com>
2018-05-01 19:33:39 +1000

    video list item displayed

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 // test return of VideoListItem
-const VideoListItem = (props) => {
-  // variable video to pull off video from props object
-  const video = props.video
-  return <li>Video</li>
+// {video} is the same as
+// const video = props.video
+const VideoListItem = ({video}) => {
+  // console.log(video)
+  // save url from current video object
+  const imageUrl = video.snippet.thumbnails.default.url
+  return (
+  <li className="list-group-item">
+    <div className="video-list media">
+      
+      <div className="media-left">
+        <img className="media-object" src={imageUrl}/>
+      </div>
+
+      <div className="media-body">
+        <div className="media-heading">{video.snippet.title}</div>
+      </div>
+
+          
+    </div>
+  </li>
+  );
 }
 
 export default VideoListItem;
